46adb26cc2f92be39b73852200063c83f17b626a
devho813 <devho813@gmail.com>
2021-02-26 17:41:56 +0900

    atom 세팅

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import type { AppProps } from 'next/app'
+import { RecoilRoot } from 'recoil'
 import { GlobalStyles } from '../utils/GlobalStyles'
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <>
       {GlobalStyles}
-      <Component {...pageProps} />
+      <RecoilRoot>
+        <Component {...pageProps} />
+      </RecoilRoot>
     </>
   )
 }
